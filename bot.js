c97f7fb424f8a7d89b0403d11c8278c587515bd9
Korawit <korawit.p@gmail.com>
2018-02-04 17:17:50 +0700

    Bot basic function

---
+++ bot.js
@@ -0,0 +1,27 @@
+const linebot = require('../index.js');
+const express = require('express');
+
+const bot = linebot({
+    channelId: process.env.CHANNEL_ID,
+    channelSecret: process.env.CHANNEL_SECRET,
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+});
+
+const app = express();
+
+const linebotParser = bot.parser();
+
+app.post('/linewebhook', linebotParser);
+
+bot.on('message', function (event) {
+    var reply = 'You say : ' + event.message.text
+    event.reply(reply).then(function (data) {
+        console.log('Success', data);
+    }).catch(function (error) {
+        console.log('Error', error);
+    });
+});
+
+app.listen(process.env.PORT || 80, function () {
+    console.log('LineBot is running.');
+});
